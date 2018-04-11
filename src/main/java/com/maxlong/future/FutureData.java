28067f6d17e2cff3cf7e390f9c427d1bddc5e474
ma.xl <ma.xl@sand.com.cn>
2018-04-11 14:37:14 +0800

    Future

--- src/main/java/com/maxlong/future/FutureData.java
+++ src/main/java/com/maxlong/future/FutureData.java
@@ -9,8 +9,29 @@ public class FutureData  implements Data{
 
     protected RealData realData = null;
 
+    protected boolean isReady = false;
+
     @Override
-    public String getResult() {
-        return null;
+    public synchronized String getResult() {
+        while (!isReady){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return realData.getResult();
     }
+
+    public synchronized void setRealData(RealData realData){
+        if(isReady){
+            return;
+        }
+
+        this.realData = realData;
+        this.isReady = true;
+        notifyAll();
+        return;
+    }
+
 }
---
+++ src/main/java/com/maxlong/future/FutureTest.java
@@ -0,0 +1,25 @@
+package com.maxlong.future;
+
+/**
+ * @author 作者 maxlong:
+ * @version 创建时间：2018-04-11 0011 14:17
+ * 类说明
+ */
+public class FutureTest {
+
+    public static void main(String[] args) {
+
+        Client client = new Client();
+
+        FutureData realData = client.request("name");
+        System.out.println("请求完毕！");
+
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("请求结果：" + realData.getResult());
+    }
+}
