f1435b46f6dd990039defcc6a6b729c8d1aa2b2c
ma.xl <ma.xl@sand.com.cn>
2018-04-11 09:25:02 +0800

    删除

---
+++ src/main/java/com/maxlong/future/FutureData.java
@@ -0,0 +1,16 @@
+package com.maxlong.future;
+
+/**
+ * @author 作者 maxlong:
+ * @version 创建时间：2018-02-24 0024 13:56
+ * 类说明
+ */
+public class FutureData  implements Data{
+
+    protected RealData realData = null;
+
+    @Override
+    public String getResult() {
+        return null;
+    }
+}
