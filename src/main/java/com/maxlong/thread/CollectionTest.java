7bbd24926877b32c2631e8ac0a1bb0ad0201521d
ma.xl <ma.xl@sand.com.cn>
2018-04-19 09:33:11 +0800

    syn

---
+++ src/main/java/com/maxlong/thread/CollectionTest.java
@@ -0,0 +1,36 @@
+package com.maxlong.thread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author 作者 maxlong:
+ * @version 创建时间：2018-04-18 0018 18:33
+ * 类说明
+ */
+public class CollectionTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService poll = Executors.newFixedThreadPool(4);
+
+//        List<String> coll = new ArrayList<>();
+        Vector<String> coll = new Vector<>();
+
+        Worker worker1 = new Worker(coll);
+        Worker worker2 = new Worker(coll);
+        Worker worker3 = new Worker(coll);
+        Worker worker4 = new Worker(coll);
+        poll.execute(worker1);
+        poll.execute(worker2);
+        poll.execute(worker3);
+        poll.execute(worker4);
+
+        poll.shutdown();
+        while (!poll.isTerminated()){}
+        System.out.println("All is finished!");
+    }
+}
