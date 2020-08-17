317a54711b6d729f75a0b0d7830ec3e6446f53e3
moore.ma <moore.ma@orderplus.com>
2020-08-17 20:37:56 +0800

    commit

--- src/main/java/com/maxlong/study/algorithm/Exercises.java
+++ src/main/java/com/maxlong/study/algorithm/Exercises.java
@@ -13,8 +13,127 @@ import java.util.*;
 public class Exercises {
 
     public static void main(String[] args) throws IOException {
-        binOneNum();
+        knapsack();
     }
+
+    /**
+     * 王强今天很开心，公司发给N元的年终奖。王强决定把年终奖用于购物，他把想买的物品分为两类：主件与附件，附件是从属于某个主件的，下表就是一些主件与附件的例子：
+     * 主件	附件
+     * 电脑	打印机，扫描仪
+     * 书柜	图书
+     * 书桌	台灯，文具
+     * 工作椅	无
+     * 如果要买归类为附件的物品，必须先买该附件所属的主件。每个主件可以有 0 个、 1 个或 2 个附件。附件不再有从属于自己的附件。王强想买的东西很多，为了不超出预算，他把每件物品规定了一个重要度，分为 5 等：用整数 1 ~ 5 表示，第 5 等最重要。他还从因特网上查到了每件物品的价格（都是 10 元的整数倍）。他希望在不超过 N 元（可以等于 N 元）的前提下，使每件物品的价格与重要度的乘积的总和最大。
+     *     设第 j 件物品的价格为 v[j] ，重要度为 w[j] ，共选中了 k 件物品，编号依次为 j 1 ， j 2 ，……， j k ，则所求的总和为：
+     * v[j 1 ]*w[j 1 ]+v[j 2 ]*w[j 2 ]+ … +v[j k ]*w[j k ] 。（其中 * 为乘号）
+     *     请你帮助王强设计一个满足要求的购物单。
+     *
+     * 输入描述:
+     *  输入的第 1 行，为两个正整数，用一个空格隔开：N m
+     * （其中 N （ <32000 ）表示总钱数， m （ <60 ）为希望购买物品的个数。）
+     *  从第 2 行到第 m+1 行，第 j 行给出了编号为 j-1 的物品的基本数据，每行有 3 个非负整数 v p q
+     * （其中 v 表示该物品的价格（ v<10000 ）， p 表示该物品的重要度（ 1 ~ 5 ）， q 表示该物品是主件还是附件。如果 q=0 ，表示该物品为主件，如果 q>0 ，表示该物品为附件， q 是所属主件的编号）
+     *
+     * 输出描述:
+     *  输出文件只有一个正整数，为不超过总钱数的物品的价格与重要度乘积的总和的最大值（ <200000 ）。
+     *
+     **/
+    public static void knapsack() throws IOException {
+        BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        while ((str = b.readLine()) != null) {
+            String[] line1 = str.split(" ");
+            Integer money = Integer.valueOf(line1[0]);
+            Integer num = Integer.valueOf(line1[1]);
+            Map<Integer, Good> goodMap = new HashMap<>();
+
+            for (int i = 0; i < num; i++) {
+                String[] line = b.readLine().split(" ");
+                Integer price = Integer.valueOf(line[0]);
+                Integer weight = Integer.valueOf(line[1]);
+                Integer index = Integer.valueOf(line[2]);
+                Good good;
+                if(index == 0){
+                    good = new Good(i + 1, price, weight);
+                } else {
+                    good = goodMap.get(index);
+                    if(good != null){
+                        good.addAnnex(price, weight);
+                    }
+                }
+                if(good != null){
+                    if(good.getTotalPrice() <= money){
+                        goodMap.put(good.index, good);
+                    } else {
+                        goodMap.remove(good.index);
+                    }
+                }
+            }
+            List<Good> goods = new ArrayList<>(goodMap.values());
+            goods.sort((g1, g2) -> {
+                if(g2.getWeightPriceRate() - g1.getWeightPriceRate() > 0){
+                    return 1;
+                } else {
+                    return -1;
+                }
+            });
+            Integer currPrice = 0;
+            Integer costWeight = 0;
+            for (Good good : goods) {
+                if(currPrice + good.getTotalPrice() > money) break;
+                costWeight += good.getAllCostWeight();
+                currPrice += good.getTotalPrice();
+            }
+            System.out.println(costWeight);
+        }
+    }
+
+    public static class Good {
+
+        private Integer index;
+
+        private Integer price;
+
+        private Integer weight;
+
+        private List<Good> annex = new ArrayList<>();
+
+        public Good(Integer index, Integer price, Integer weight) {
+            this.index = index;
+            this.price = price;
+            this.weight = weight;
+        }
+
+        public void addAnnex(Integer price, Integer weight){
+            this.annex.add(new Good(this.index, price, weight));
+        }
+
+        public double getWeightPriceRate(){
+            return getAllCostWeight()/getTotalPrice();
+        }
+
+        public Integer getTotalPrice(){
+            Integer price = this.price;
+            for (Good good : annex) {
+                price += good.price;
+            }
+            return price;
+        }
+
+        public Integer getAllCostWeight(){
+            Integer costWeight = this.price * weight;
+            for (Good good : annex) {
+                costWeight += good.getSelfCostWeight();
+            }
+            return costWeight;
+        }
+
+        public Integer getSelfCostWeight(){
+            return this.price * weight;
+        }
+
+    }
+
 
     /**
      * 输入一个int型的正整数，计算出该int型数据在内存中存储时1的个数。
